9ea7592bbef747ac4a0db2875c50740188a19068
John Wilson <johnthewilson@gmail.com>
2016-02-13 20:04:00 -0800

    Oauth fixed

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,10 @@ var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 var IntuitStrategy = require('passport-intuit-oauth').Strategy
 var User = require('../models/User');
 
-var secrets = require('secrets');
+var secrets = require('./secrets');
+
+var INTUIT_CONSUMER_KEY = "qyprdsRlGX5ZlnKq03lWgHeSWyrmfl";
+var INTUIT_CONSUMER_SECRET = "Ij3fAwUmBYIr2PMU0uWYt0fCU390obyK0hKou06e";
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -59,9 +62,9 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, passw
 * Sign in with Intuit.
 */
 passport.use(new IntuitStrategy({
-    consumerKey: secrets.intuit.consumerKey,
-    consumerSecret: secrets.intuit.consumerSecret,
-    callbackURL: secrets.intuit.callbackURL
+    consumerKey: INTUIT_CONSUMER_KEY,
+    consumerSecret: INTUIT_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/intuit/callback"
   },
   function(token, tokenSecret, profile, done) {
     User.findOrCreate({ intuitId: profile.id }, function (err, user) {
@@ -69,63 +72,6 @@ passport.use(new IntuitStrategy({
     });
   }
 ));
-
-
-/**
- * Sign in with Facebook.
- */
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_ID,
-  clientSecret: process.env.FACEBOOK_SECRET,
-  callbackURL: '/auth/facebook/callback',
-  profileFields: ['name', 'email', 'link', 'locale', 'timezone'],
-  passReqToCallback: true
-}, function(req, accessToken, refreshToken, profile, done) {
-  if (req.user) {
-    User.findOne({ facebook: profile.id }, function(err, existingUser) {
-      if (existingUser) {
-        req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, function(err, user) {
-          user.facebook = profile.id;
-          user.tokens.push({ kind: 'facebook', accessToken: accessToken });
-          user.profile.name = user.profile.name || profile.displayName;
-          user.profile.gender = user.profile.gender || profile._json.gender;
-          user.profile.picture = user.profile.picture || 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-          user.save(function(err) {
-            req.flash('info', { msg: 'Facebook account has been linked.' });
-            done(err, user);
-          });
-        });
-      }
-    });
-  } else {
-    User.findOne({ facebook: profile.id }, function(err, existingUser) {
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
-        if (existingEmailUser) {
-          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
-          done(err);
-        } else {
-          var user = new User();
-          user.email = profile._json.email;
-          user.facebook = profile.id;
-          user.tokens.push({ kind: 'facebook', accessToken: accessToken });
-          user.profile.name = profile.displayName;
-          user.profile.gender = profile._json.gender;
-          user.profile.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-          user.profile.location = (profile._json.location) ? profile._json.location.name : '';
-          user.save(function(err) {
-            done(err, user);
-          });
-        }
-      });
-    });
-  }
-}));
 
 
 
--- config/secrets.js
+++ config/secrets.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     intuit: {
-    	consumerKey: 'SECRET',
-    	consumerSecret:'SECRET',
-    	callbackURL: '/auth/intuit/callback',
+    	consumerKey: 'qyprdsRlGX5ZlnKq03lWgHeSWyrmflCopy',
+    	consumerSecret:'Ij3fAwUmBYIr2PMU0uWYt0fCU390obyK0hKou06e',
+    	callbackURL: 'http://127.0.0.1:3000/auth/intuit/callback',
     	passReqToCallback: true
     }
 };
